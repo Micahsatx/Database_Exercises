d1c73fa66201e882705c46db6a2c30ced103efb7
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-05 15:45:28 -0500

    8.3 mysql creating a table exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
- USE codeup_test_db;
+USE codeup_test_db;
+
+DROP TABLE if exists albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED AUTO_INCREMENT, 
+    artist VARCHAR(50),
+    record VARCHAR(100),
+    release_date YEAR,
+    sales FLOAT,
+    genre VARCHAR(150),
+    PRIMARY KEY (id)
+);
