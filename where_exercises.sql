efcb3d2efc64740297341562b58ee4596fd88089
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-09 10:36:11 -0500

    8.7.3 mysql pro ordering querries

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,17 +5,20 @@ AND (
 first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya'
-); 
+)
+ORDER BY first_name DESC, last_name DESC; 
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-AND last_name LIKE '%E';
+AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
 
-SELECT emp_no, first_name, last_name
+SELECT emp_no, first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC ;
 
 SELECT emp_no, first_name, last_name
 FROM employees
