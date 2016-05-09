b8a468dc72a7ba80db9f8fb1124976eee0369aa8
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-09 10:17:28 -0500

    8.7.2 sql pro more querries

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,14 +1,21 @@
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya'); 
+WHERE gender = 'M'
+AND (
+first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya'
+); 
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT emp_no, first_name, last_name
 FROM employees
@@ -16,4 +23,5 @@ WHERE birth_date LIKE '%-12-25';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name NOT LIKE '%qu%'
+AND last_name LIKE '%q%';
